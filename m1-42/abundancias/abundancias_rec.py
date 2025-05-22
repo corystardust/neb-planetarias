1221cf52df0d972f47114c0c555d17e28b0af300
Lesly Corina Castañeda <53490924+corystardust@users.noreply.github.com>
2025-05-22 16:13:40 -0600

    Update abundancias_rec.py

--- m1-42/abundancias/abundancias_rec.py
+++ m1-42/abundancias/abundancias_rec.py
@@ -1,6 +1,6 @@
 import astropy
 import numpy as np
-import matplotlib.pyplot as plt  
+import matplotlib.pyplot as plt  # Cambiar a pyplot en lugar de pylab
 import scipy
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import pyneb as pn
@@ -14,28 +14,20 @@ cmap_name = 'custom_cmap'
 newcmp = LinearSegmentedColormap.from_list(cmap_name, color_list, N=256)
 
 inicio = time.time()
-temp= fits.open('/Users/leslycorina/iraf/archive/m1-42/Temp/tem_4363c_4959c.fits')
-temp_oiii= temp[0].data
-temp_header= temp[0].header
-
-
-imagen = fits.open('OIII4959cnsv4_hbetapv4.fits')
+imagen = fits.open('OII4649cpv4_hbetansv4.fits')
 datos= imagen[0].data
 header= imagen[0].header
-
 extent_normal = [-165,-35,-24,24] # Extent en coordenadas físicas
 
-O3 = pn.Atom('O', 3)
+O2 = pn.RecAtom('O', 2)
 nr = datos.shape[0]  # guardamos el número de filas
 nf = datos.shape[1]  # número de columnas
 ionabun = np.zeros(shape=(nr, nf)) 
-
 # Ahora hay que realizar un bucle que calcule la temperatura en cada fila y columna y lo guarde en la matriz de ceros
 for i in range(nr):
     for j in range(nf):
-        abundance = O3.getIonAbundance(int_ratio=datos[i, j], tem=temp_oiii[i,j], den=1000., wave=4959, Hbeta=1.)
+        abundance = O2.getIonAbundance(int_ratio=datos[i, j], tem=2000, den=7500., wave=4649.13, Hbeta=1.)
         ionabun[i, j] = abundance
-
 
 print('Habemus mapita PV')
 
@@ -44,7 +36,7 @@ print('Habemus mapita PV')
 fig, ax = plt.subplots(figsize=(8,4))
 
 # Mostrar la imagen con escala logarítmica
-im = ax.imshow(12 + np.log10(ionabun), cmap=newcmp, aspect='auto', origin='lower',extent=extent_normal,vmin=7.8, vmax= 8.6)
+im = ax.imshow(12 + np.log10(ionabun), cmap=newcmp, aspect='auto', origin='lower',extent=extent_normal,vmin=9.4, vmax= 10.2)
 
 # Crear un eje extra para la barra de color usando make_axes_locatable
 divider = make_axes_locatable(ax)
@@ -55,20 +47,22 @@ cbar = fig.colorbar(im, cax=cax)
 #cbar.set_label('O$^{++}$/H$^{+}$')
 
 # Agregar título y etiquetas
-#ax.set_title('Abundancia de O$^{++}$ CEL')
-ax.text(-160, 19,'O$^{2+}$ $\\lambda$4959/H$\\beta$', fontsize=12, bbox=dict(facecolor='white'))
+#ax.set_title('Abundancia de O$^{++}$ add RL')
+ax.text(-160, 19,'O$^{2+}$ $\\lambda$4649/H$\\beta$', fontsize=12, bbox=dict(facecolor='white'))
 ax.set_xlabel('V (km/s)')
-ax.set_ylabel('Spatial axis')
+ax.set_ylabel('Spatial axis (arc sec)')
 ax.axhline(y=0,linewidth=0.9, color='black')  
 
-plt.savefig("abundances_OIII4959nsv4_hbetapnsv4.png", dpi=150, overwrite=True)
+# Guardar la figura
+plt.savefig("abundances_OII4649c_hbetapnsv4.png", dpi=150, overwrite=True)
+
+# Mostrar la imagen
 plt.show()
-# Mostrar la gráfica
 
 
 imf = fits.PrimaryHDU()
 imf.data = ionabun
-imf.writeto("abundances_OIII4959nsv4_hbetapnsv4.fits", overwrite=True)
+imf.writeto("abundances_OII4649cpv4_Hbetapnsv4.fits", overwrite=True)
 
 fin = time.time()
 print(fin-inicio)
